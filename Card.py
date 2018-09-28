1e9a71f2c774651da6b0368adc950fd3e7156fef
David Gurevich <david@gurevich.ca>
2018-09-28 09:02:45 -0400

    Template commit

---
+++ Card.py
@@ -0,0 +1,35 @@
+"""
+Name: Card.py
+Author:
+Init Date: 28 September, 2018
+
+Latest Edit: 28 September, 2018
+Edited by: David Gurevich
+
+{Documentation goes here}
+"""
+
+import pygame
+
+class Card(object):
+    def __init__(self, col, row, id):
+        self._col = col
+        self._row = row
+
+        self._id = id
+
+    def draw(self, screen):
+        pass
+        
+    # Getters and Setters example
+    def get_col(self):
+        return self._col
+
+    def set_col(self, col):
+        self._col = col
+
+    def get_row(self):
+        return self._row
+
+    def set_row(self):
+        self._row = row
---
+++ MemoryGame.py
@@ -0,0 +1,22 @@
+"""
+Name: MemoryGame.py
+Author:
+Init Date: 28 September, 2018
+
+Latest Edit: 28 September, 2018
+Edited by: David Gurevich
+
+{Documentation goes here}
+"""
+
+import pygame
+
+# Importing Card class from file 'Card.py'
+from Card import Card
+
+
+def main_loop():
+    pass
+
+if __name__ == '__main__':
+    main_loop()
