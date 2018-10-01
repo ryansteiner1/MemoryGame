bcbc61dbbcaca80054f1478a8932fe449e40f4dd
ryansteiner1 <43676344+ryansteiner1@users.noreply.github.com>
2018-10-01 09:53:34 -0400

    Update Card.py

--- Card.py
+++ Card.py
@@ -3,11 +3,8 @@ Name: Card.py
 Author:
 Init Date: 28 September, 2018
 
-Latest Edit: 28 September, 2018
-Edited by: David Gurevich
-
-Finally worked: 28, September, 2018
-Time: 9:5
+Latest Edit: 1 October, 2018
+Edited by: Ryan Steiner
 
 {Documentation goes here}
 """
@@ -15,26 +12,50 @@ Time: 9:5
 import pygame
 
 class Card(object):
-    def __init__(self, col, row, id):
-        self._col = col
-        self._row = row
+    def __init__(self, col, row, num, active, flipped):
+        self._cord = (col, row)
+        self._num = num
+        self._image = pygame.image.load("assets/%s") % str(self._num)
+        self._cardback = pygame.image.load("assets/Card.png")
+        self._flipped = False
+        self._active = True
 
-        self._id = id
-
-    def draw(self, screen):
-        pass
-        
-    # Getters and Setters example
-    def get_col(self):
-        return self._col
-
-    def set_col(self, col):
-        self._col = col
-
-    def get_row(self):
-        return self._row
-
-    def set_row(self):
-        self._row = row
-
+    def restart(self):
+        self._flipped = False
+        self._actve = True
     
+    def draw(self, screen):
+        if self._flipped:
+            screen.blit(self._image,(self.col,self._row))
+        else:
+            screen.blit(self._cardback,(self._col,self._row))
+            
+    def get_cord(self):
+        return self._cord
+
+    def set_cord(self,cord):
+        self._cord = cord
+
+    def get_num(self):
+        return self._num
+
+    def set_num(self):
+        self._num = num
+
+    def get_flipped(self):
+        return self._flipped
+
+    def set_flipped(self,flipped):
+        self._flipped = fipped
+
+    def get_active(self):
+        return self._active
+
+    def set_active(self,active):
+        self._active = active
+
+    def __eq__(self,other):
+        if self.get_num == other.get_num:
+            return True
+        else:
+            return False
